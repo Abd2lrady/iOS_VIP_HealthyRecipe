65c9200af764b8f86b925f8003b92cdb1c829b8f
Abd2lrady <abd2lrady@gmail.com>
2022-11-11 03:05:52 +0200

    removing main storyBorad and booting app from SceneDelegate

--- iOS_Recipe/AppDelegate + SceneDelegate/SceneDelegate+Extension.swift
+++ iOS_Recipe/AppDelegate + SceneDelegate/SceneDelegate+Extension.swift
@@ -5,8 +5,17 @@
 //  Created by Ahmad Abdulrady on 11/11/2022.
 //
 
-import Foundation
+import UIKit
 
 extension SceneDelegate {
-    
+    func bootApp(scene: UIWindowScene) {
+        let window = UIWindow(windowScene: scene)
+        let view = ViewController()
+        let navigator = UINavigationController(rootViewController: view)
+
+        window.rootViewController = navigator
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+
 }
