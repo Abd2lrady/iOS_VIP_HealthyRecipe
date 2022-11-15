fcc5f84fafdf4c09fe9f15ea556815d81b6428ea
Abd2lrady <abd2lrady@gmail.com>
2022-11-15 17:10:46 +0200

    adding Filter.swift file

---
+++ iOS_Recipe/Modules/Models/Entities/Filter.swift
@@ -0,0 +1,14 @@
+//
+//  Filter.swift
+//  iOS_Recipe
+//
+//  Created by Ahmad Abdulrady on 11/11/2022.
+//
+
+import Foundation
+
+enum Filter: String {
+    case lowSugar = "low-sugar"
+    case keto = "keto-friendly"
+    case vegan = "vegan"
+}
